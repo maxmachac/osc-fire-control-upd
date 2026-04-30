4230c88111c4ce18ff6c9213dd1bbb6a1020609b
maxmachac <155898864+maxmachac@users.noreply.github.com>
2026-04-30 08:42:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Názov tvojho repozitára s lomkami na oboch stranách
+  base: '/osc-fire-control-upd/', 
   plugins: [react()],
 })
 
